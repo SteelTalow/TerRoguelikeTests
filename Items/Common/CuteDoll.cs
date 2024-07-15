e7ff8a1f2d502d4d337ec31ac8f41c2923a716e2
SteelTalow <steeltalonbwb2w2hgss@gmail.com>
2024-07-14 20:28:15 -0500

    Create CuteDoll.cs

---
+++ Items/Common/CuteDoll.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TerRoguelike.TerPlayer;
+
+namespace TerRoguelike.Items.Common
+{
+    public class AmberBead : BaseRoguelikeItem, ILocalizedModType
+    {
+      //changed: name
+        public override int modItemID => ModContent.ItemType<CuteDoll>();
+        public override bool HealingItem => false;
+        public override bool UtilityItem => true;
+        public override int itemTier => 0;
+        public override float ItemDropWeight => 0.5f;
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.rare = ItemRarityID.Blue;
+            Item.maxStack = Item.CommonMaxStack;
+        }
+        public override void ItemEffects(Player player)
+        {
+            player.GetModPlayer<TerRoguelikePlayer>().cuteDoll += Item.stack;
+        }
+    }
+}
